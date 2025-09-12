198256fc6fe37210bb915d443baf75aed3de1d42
tomasviana16 <tomasvianarf@gmail.com>
2025-09-12 12:16:50 -0300

    implementacão dos tads no main

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "Model TAD_Pacote/TAD_Pacote.h"
+#include "Model TAD_Lista_Pacote/TAD_Lista_Pacotes.h"
+#include "Model TAD_Drone/TAD_Drone.h"
+#include "Model TAD_Galpão/TAD_Galpao.h"
+
+int main(){
+
+}
